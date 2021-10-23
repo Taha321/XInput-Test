45c8db446ff2758c65ad8fe0d5cced380b7fa7b4
Taha321 <93052614+Taha321@users.noreply.github.com>
2021-10-23 18:08:42 -0400

    Update Test.ino

--- Test.ino
+++ Test.ino
@@ -4,17 +4,14 @@
 
 void setup() 
 {
-  // put your setup code here, to run once:
   XInput.begin();
   pinMode(14,INPUT_PULLDOWN);
   pinMode(13,OUTPUT);
-  Serial.begin(9600);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-  int val = digitalRead(14);
+  bool val = digitalRead(14);
 
   if(val == true)
   {
